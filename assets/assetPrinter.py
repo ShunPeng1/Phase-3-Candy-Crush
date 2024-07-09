8ae66208c46829f8b30fbc449741c49d2ae74398
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2024-07-09 14:03:01 +0700

    feat: pack json for asset

---
+++ assets/assetPrinter.py
@@ -0,0 +1,24 @@
+import os
+
+# Step 2: Define the directory
+directory = "./assets/candy-crush/images"
+
+# Step 3: List files in the directory
+files = os.listdir(directory)
+
+# Step 4: Filter for PNG files
+png_files = [file for file in files if file.endswith('.png')]
+
+# Step 5 & 6: Generate and print the desired text and rename files
+for file in png_files:
+    key = file.split('.')[0]  # Remove the file extension to get the key
+    key_with_dashes = key.replace("_", "-")  # Replace underscores with dashes
+    new_file_name = key_with_dashes + ".png"  # Construct new file name with dashes
+    print(f"""      {{
+        "type": "image",
+        "key": "{key_with_dashes}",
+        "url": "{directory}/{new_file_name}"
+      }},""")
+    
+    # Rename the file
+    os.rename(os.path.join(directory, file), os.path.join(directory, new_file_name))
